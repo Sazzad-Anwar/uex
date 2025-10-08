4937a4d9e34a8df5941774dba573b1be3b8829a0
sazzad-anwar <sazzadzihan@gmail.com>
2025-10-09 01:50:57 +0600

    Add SVG icons for search and verified functionalities
    - Added a new search icon (search.svg) to enhance the search feature.
    - Introduced a verified icon (verified.svg) for indicating verified status.

--- src/main.js
+++ src/main.js
@@ -19,3 +19,68 @@ window.addEventListener('load', () => {
 window.addEventListener('resize', () => {
   setHeroBackground()
 })
+
+document.addEventListener('alpine:init', () => {
+  Alpine.store('comparison', {
+    activeTab: 1,
+    tabs: [
+      {
+        id: 1,
+        title: 'UEX Flexible',
+        data: [
+          {
+            usdYield: '4.5%',
+            cryptoYield: '5.5%',
+            lockUps: 'None',
+            withdrawAnytime: true,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'UEX Premium',
+      },
+      {
+        id: 3,
+        title: 'Traditional Bank',
+      },
+    ],
+  }),
+    Alpine.store('dailySavings', {
+      faq: [
+        {
+          title: 'U.S. MSB-registered.',
+          description:
+            'Grog seven yer locker overhaul crack. Tales black hang me coast gar. Parrel splice hearties or nipperkin bucko. Tea gabion prey blow seven dead. Lubber.',
+          image: './public/images/verified.svg',
+          isOpen: true,
+        },
+        {
+          title:
+            'Institutional-grade custody: multi-sig cold storage, encrypted transactions.',
+          description:
+            'Grog seven yer locker overhaul crack. Tales black hang me coast gar. Parrel splice hearties or nipperkin bucko. Tea gabion prey blow seven dead. Lubber.',
+          image: './public/images/lock.svg',
+          isOpen: false,
+        },
+        {
+          title:
+            'Yields powered by collateralized loans and hedging — not risky DeFi farms',
+          description:
+            'Grog seven yer locker overhaul crack. Tales black hang me coast gar. Parrel splice hearties or nipperkin bucko. Tea gabion prey blow seven dead. Lubber.',
+          image: './public/images/hand.svg',
+          isOpen: false,
+        },
+        {
+          title: 'Continuous audits + segregated funds',
+          image: './public/images/search.svg',
+          description:
+            'Grog seven yer locker overhaul crack. Tales black hang me coast gar. Parrel splice hearties or nipperkin bucko. Tea gabion prey blow seven dead. Lubber.',
+          isOpen: false,
+        },
+      ],
+      toggle(index) {
+        this.faq[index].isOpen = !this.faq[index].isOpen
+      },
+    })
+})
